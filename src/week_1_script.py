b13f6448415130696c046620214586f83c7bda15
vapmail16 <vapmail16@gmail.com>
2025-03-10 17:12:04 +0000

    Updated CI workflow and scripts

---
+++ src/week_1_script.py
@@ -0,0 +1,18 @@
+# This is Week 1 script. 
+
+# This is Day 1 Script - It prints "Hello World!" to the console.
+
+print("Hello World!")
+
+print("My Name is Vikkas Arun Pareek")
+print("I am learning Python Programming Language")
+print("80-20 rule of learning, focus on essentions")
+print("My Favourite Quote is: Rise and Shine")
+print("Now I will show you ASCII Art")
+print("   *   ")
+print("  ***  ")
+print(" ***** ")
+print("*******")
+
+
+# This is Day 2 Script - 
