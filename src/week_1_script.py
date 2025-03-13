3ce783f523069c303d629cb3ec13fc58a1bbb723
vapmail16 <vapmail16@gmail.com>
2025-03-13 00:07:11 +0000

    Add week_1_script.py with more conditional Use Cases

--- src/week_1_script.py
+++ src/week_1_script.py
@@ -151,8 +151,91 @@ else:
 
 
 
+# 4 FizzBuzz Problem:
 
+
+   
+if not (is_ci or is_test):
+    fizz_num = int(input("Enter a number: "))
+else: 
+    fizz_num = 2
+
+    if fizz_num % 3 == 0:
+        print ("Fizz")
+    elif fizz_num % 5 == 0:
+        print ("Buzz")
+    elif fizz_num % 3 == 0 and fizz_num % 5 == 0:
+        print ("FizzBuzz")
+    else:
+        print(fizz_num)
+        
+
+
+
+# Factorial Calculator (Using Loops)
+
+if not (is_ci or is_test):
+ factorial_num = int(input("Enter a number: "))
+else:
+ factorial_num = 5
+
+fact_num = 1
+for i in range(1, factorial_num+1):
+     fact_num *= i 
+print(f"Factorial of {factorial_num} is {fact_num}")
+
+
+
+# Sum of Digits
+
+if not (is_ci or is_test):
+    num_sum = int(input("Enter a number: "))
+else:
+    num_sum = 123    
+
+sum = 0 
+while num_sum > 0:
+    sum += num_sum % 10
+    num_sum = num_sum // 10
+print(f"Sum of digits is {sum}") 
+
+#What happens if the user enters a negative number? Can you handle it?
+#How can you solve this using string manipulation instead of math?
+#What if the number contains leading zeros (e.g., 0123)? Does it matter?
+
+
+
+# Reverse a String
+
+
+if not (is_ci or is_test):
+    string = input("Enter a string: ")
+    reverse_string = string[::-1]
+    print(f"Reverse of {string} is {reverse_string}")
+else:
+    name = "TestUser"
+
+
+
+# Guess the Number Game
+
+set_num = 10
+
+while True:
+    if not (is_ci or is_test):
+        ran_num = int(input("Enter a number: "))
+    else:
+        ran_num = 10    
+
+    if ran_num == set_num:
+     print("You guessed it!")  
+     break
+
+    elif abs(ran_num - set_num) <= 2:
+     print("Close!")
+
+    else:
+     print("Not Close")
+
+ 
 # This is Day 4 Script -
-
-
-
